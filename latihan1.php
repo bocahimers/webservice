5280eb59d238b23833e7c7deee89ba7a18029527
wahyusoft <wahyusoft@yahoo.com>
2017-09-29 19:44:20 +0700

    Latihan pertemuan 1

--- latihan1.php
+++ latihan1.php
@@ -1,8 +1,19 @@
 <?php
-$url = "http://stmikelrahma.ac.id";
-$ch = curl_init($url);
-curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
-$result = curl_exec($ch);
-curl_close($ch);
-echo "<pre>";print_r($result);
+function get_web_page( $url )
+{
+    $options = [
+        CURLOPT_CUSTOMREQUEST  =>"GET",    // Atur type request, 		                                   // get atau post
+        CURLOPT_POST           => false,   // Atur menjadi GET
+        CURLOPT_FOLLOWLOCATION => true,    // Follow redirect aktif
+        CURLOPT_CONNECTTIMEOUT => 120,     // Atur koneksi timeout
+        CURLOPT_TIMEOUT        => 120,     // Atur response timeout
+    ];
+		$ch = curl_init( $url );        // Inisialisasi Curl
+		curl_setopt_array( $ch, $options );  // Set Opsi
+        $content = curl_exec( $ch );         // Eksekusi Curl
+        curl_close( $ch );                   // Stop atau tutup script
+        $header['content'] = $content;
+    return $header;
+ }
+  echo get_web_page("http://stmikelrahma.ac.id");
 ?>
